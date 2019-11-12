44ab594c77fe8f53258df237e9daa37439019f76
Conor Ryan <cr.in.france@gmail.com>
2019-11-12 12:51:44 +0100

    Parsing CSV

--- read_data.py
+++ read_data.py
@@ -1,5 +1,53 @@
 from enum import Enum
 
+
+class Race(Enum):
+	Protoss = 0
+	Zerg = 1
+	Terran = 2
+
+
+class HotkeyAction(Enum):
+	created = 0
+	updated = 1
+	used = 2
+
+
+class Hotkey:
+	key: int
+	action: HotkeyAction
+
+
+class ActionType:
+	Selection = 0
+	Base = 1
+	Mineral = 2
+	Hotkey = 3
+
+
+class Action:
+	type: ActionType
+	hotkey: Hotkey
+
+	def __init__(self, type):
+		self.type = type
+
+
+class ActionInterval:
+	start: int
+	actions: [Action]
+
+	def __init__(self, start):
+		self.start = start
+		self.actions = []
+
+
+class Game:
+	playerId: str
+	race: Race
+	intervals: [ActionInterval]
+
+
 '''
 FEATURES
 - played race
@@ -9,40 +57,45 @@ FEATURES
 - first hotkey used
 - nb created hotkey 5 first seconds
 - nb base and mineral selected
-
 '''
-class Race(Enum):
-	Protoss = 0
-	Zerg = 1
-	Terran = 2
 
-class HotkeyAction(Enum):
-	created = 0
-	updated = 1
-	used = 2
-
-class Hotkey:
-	key: int
-	action: HotkeyAction
-
-class Action:
-	selection: str
-	hotkey: str
-
-class TimedAction:
-	start: int
-	end: int
-	actions: [Action]
-
-class Player:
-	race: Race
-	timed_actions: [TimedAction]
-
-with open("data/TRAIN.csv", "r") as file:
+with open("data/TRAIN.CSV", "r") as file:
 	# lines_data = [line.split(',') for line in file]
 
-	data = []
+	games: [Game] = []
 	for line in file:
 		line_data = line.split(',')
-		print(line_data)
-		break
+
+		g = Game()
+		g.playerId = line_data[0]
+		g.race = line_data[1]
+		g.intervals = []
+
+		interval = ActionInterval(0)
+		g.intervals.append(interval)
+		for i in range(2, len(line_data)):
+			line = line_data[i].rstrip()
+			# New interval
+			if line[0] == "t":
+				interval = ActionInterval(line[1:])
+				g.intervals.append(interval)
+
+			# Selection
+			else:
+				action: Action
+				if line == "s":
+					action = Action(ActionType.Selection)
+
+				elif line == "Base":
+					action = Action(ActionType.Base)
+
+				elif line == "SingleMineral":
+					action = Action(ActionType.Mineral)
+
+				else:
+					action = Action(ActionType.Hotkey)
+					hotkey = Hotkey()
+					hotkey.key = line[6]
+					hotkey.action = HotkeyAction(int(line[7]))
+
+				interval.actions.append(action)
