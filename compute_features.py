cf9b14fca69916ec9e15f6251b1a7c94d2cb7003
Conor Ryan <cr.in.france@gmail.com>
2019-11-20 11:33:11 +0100

    91,470 baby

--- compute_features.py
+++ compute_features.py
@@ -7,12 +7,9 @@ from sklearn import preprocessing
 
 from read_data import Game, ActionInterval, read_file, ActionType, Race, HotkeyAction
 
-"""
-ideas:
-factor use/create
+# selection to key ratio
 
-"""
-onehotkey = preprocessing.OneHotEncoder()
+onehotkey = preprocessing.OneHotEncoder(categories='auto')
 X = [[x] for x in range(11)]
 onehotkey.fit(X)
 
@@ -21,7 +18,7 @@ def onehotkey_f(hotkey):
 	return onehotkey.transform([[hotkey]]).toarray()[0]
 
 
-onehotrace = preprocessing.OneHotEncoder()
+onehotrace = preprocessing.OneHotEncoder(categories='auto')
 X = [[x] for x in range(3)]
 onehotrace.fit(X)
 
@@ -32,7 +29,6 @@ def onehotrace_f(race):
 
 class Features:
 	race: Race
-	min_click: int
 	max_click: int
 	avg_click: int
 	uses_per_key: Dict[int, int]
@@ -42,14 +38,18 @@ class Features:
 	first_used_hotkey: int
 	first_created_hotkey: int
 	first_updated_hotkey: int
+	max_creations_in_row: int
+	max_uses_in_row: int
+	max_updates_in_row: int
+	base_clicks: int
+	mineral_clicks: int
 	game_length: int
+	key_use_in_row: Dict[int, int]
 
 	def to_array(self):
 		return np.concatenate(
 			[
-				onehotrace_f(self.race.value),
-				[self.max_click],
-				[self.avg_click],
+				[self.max_click, self.avg_click],
 				onehotkey_f(self.first_used_hotkey),
 				onehotkey_f(self.first_created_hotkey),
 				onehotkey_f(self.first_updated_hotkey),
@@ -57,14 +57,17 @@ class Features:
 				[self.creations_per_key[x] for x in range(10)],
 				[self.updates_per_key[x] for x in range(10)],
 				[self.ratio_use_create_and_update_per_key[x] for x in range(10)],
+				[self.max_creations_in_row, self.max_uses_in_row, self.max_updates_in_row],
+				[self.mineral_clicks, self.base_clicks],
+				[self.key_use_in_row[x] for x in range(10)]
 			]).ravel().tolist()
 
 
 class ComputeFeatures:
 
-	def __get_click_stats_5_seconds(self, intervals: [ActionInterval]):
+	def __get_action_stats_5_seconds(self, intervals: [ActionInterval]):
 		a = [len(actions_list.actions) for actions_list in intervals]
-		return min(a), max(a), sum(a) / len(intervals)
+		return max(a), sum(a) / len(intervals)
 
 	def __get_action_per_key(self, intervals: [ActionInterval], hotkeyaction: HotkeyAction):
 		dict = {}
@@ -93,10 +96,66 @@ class ComputeFeatures:
 			dict[key] = value / (feature.updates_per_key[key] + feature.creations_per_key[key] + 1)
 		return dict
 
+	def __get_max_actions_in_row(self, intervals: [ActionInterval], hotkeyaction: HotkeyAction):
+		current_n = 0
+		max_n = 0
+		for interval in intervals:
+			for action in interval.actions:
+				if action.type == ActionType.Hotkey and action.hotkey.action == hotkeyaction:
+					current_n += 1
+					max_n = max(current_n, max_n)
+				else:
+					current_n = 0
+		return max_n
+
+	def __get_max_actions_in_row_per_key(self, intervals: [ActionInterval], hotkeyaction: HotkeyAction):
+		dict = {}
+		for i in range(10):
+			dict[i] = 0
+		for interval in intervals:
+			for action in interval.actions:
+				if action.type == ActionType.Hotkey and action.hotkey.action == hotkeyaction:
+					dict[action.hotkey.key] += 1
+
+		# normalize
+		for i in range(10):
+			dict[i] /= (len(intervals) + 1)
+		return dict
+
+	def __get_max_same_key_in_row(self, intervals: [ActionInterval]):
+		current_n = 0
+		current_key = None
+		dict = {}
+		for i in range(10):
+			dict[i] = 0
+		for interval in intervals:
+			for action in interval.actions:
+				if action.type == ActionType.Hotkey:
+					if action.hotkey.key == current_key:
+						current_n += 1
+						dict[current_key] = max(dict[current_key], current_n)
+					else:
+						current_n = 1
+						current_key = action.hotkey.key
+		return dict
+
+	def __get_clicks(self, intervals: [ActionInterval]):
+		mineral_n, base_n = 0, 0
+		for interval in intervals:
+			for action in interval.actions:
+				if action.type == ActionType.Mineral:
+					mineral_n += 1
+				elif action.type == ActionType.Base:
+					base_n += 1
+
+		mineral_n = mineral_n * 100 / len(intervals)
+		base_n = base_n * 100 / len(intervals)
+		return mineral_n, base_n
+
 	def compute_features(self, game: Game) -> Features:
 		f = Features()
 		f.race = game.race
-		f.min_click, f.max_click, f.avg_click = self.__get_click_stats_5_seconds(game.intervals)
+		f.max_click, f.avg_click = self.__get_action_stats_5_seconds(game.intervals)
 		f.uses_per_key = self.__get_action_per_key(game.intervals, HotkeyAction.used)
 		f.creations_per_key = self.__get_action_per_key(game.intervals, HotkeyAction.created)
 		f.updates_per_key = self.__get_action_per_key(game.intervals, HotkeyAction.updated)
@@ -105,6 +164,11 @@ class ComputeFeatures:
 		f.first_updated_hotkey = self.__get_first_hotkey_action(game.intervals, HotkeyAction.updated)
 		f.game_length = len(game.intervals),
 		f.ratio_use_create_and_update_per_key = self.__get_ratio_use_create_and_update_per_key(f)
+		f.max_creations_in_row = self.__get_max_actions_in_row(game.intervals, HotkeyAction.created)
+		f.max_uses_in_row = self.__get_max_actions_in_row(game.intervals, HotkeyAction.used)
+		f.max_updates_in_row = self.__get_max_actions_in_row(game.intervals, HotkeyAction.updated)
+		f.mineral_clicks, f.base_clicks = self.__get_clicks(game.intervals)
+		f.key_use_in_row = self.__get_max_same_key_in_row(game.intervals)
 		return f
 
 
@@ -123,6 +187,7 @@ def get_features(filename: str, max_items: int, label_present=True) -> (List[str
 			if label_present:
 				labels.append(data.playerId)
 			features.append(c.compute_features(data).to_array())
+
 
 		with open(result_filename, "wb") as file:
 			marshal.dump({"labels": labels, "features": features}, file)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import csv
 import sys
 from typing import List
+
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split, GridSearchCV
@@ -8,7 +9,7 @@ from sklearn.model_selection import train_test_split, GridSearchCV
 from compute_features import get_features
 
 MAX_ITEMS = sys.maxsize
-RANDOM_STATE = 80085
+RANDOM_STATE = 80086
 TRAIN_DATA_PATH = "data/TRAIN.CSV"
 TEST_DATA_PATH = "data/TEST.CSV"
 SUBMIT_FILE = "data/submission.CSV"
@@ -21,12 +22,12 @@ labels, features = get_features(TRAIN_DATA_PATH, MAX_ITEMS)
 x_train, x_test, y_train, y_test = train_test_split(features, labels, random_state=RANDOM_STATE, test_size=0.2)
 
 parameters_dict = {
-	"n_estimators": [256],
-	"max_features": [x/100 for x in range(10, 60, 5)],
-	"max_depth": [x for x in range(1, 21, 1)],
-	"min_samples_split": [x for x in range(2, 8, 2)],
-	"min_samples_leaf": [x for x in range(1, 5, 1)],
 	"criterion": ["gini"],
+	"max_depth": [27],
+	"max_features": [0.055],
+	"min_samples_leaf": [1],
+	"min_samples_split": [2],
+	"n_estimators": [256],
 	"random_state": [RANDOM_STATE]
 }
 
@@ -40,14 +41,11 @@ print(search.best_params_)
 print('Accuracy Score on train data: ', accuracy_score(y_true=y_train, y_pred=search.predict(x_train)))
 print('Accuracy Score on test data: ', accuracy_score(y_true=y_test, y_pred=search.predict(x_test)))
 
-"""
-TEST FOR SUBMISSIONS
-"""
 feature_importances = list(map(lambda x: round(x, 4) * 100, search.best_estimator_.feature_importances_))
 labeled_features, count = [], 0
-agg, start = [3, 1, 1, 10, 10, 10, 10, 10, 10, 10], 0
+agg, start = [3, 2, 10, 10, 10, 10, 10, 10, 10, 3, 1, 2, 10], 0
 for i in agg:
-	for j in range(start, start+i):
+	for j in range(start, start + i):
 		count += feature_importances[j]
 	start += i
 	labeled_features.append(count)
@@ -56,8 +54,7 @@ for i in agg:
 print("------- Feature importances ------")
 print(labeled_features)
 
-
-input("Do you want to predict test with this model ? The current submission.csv will be deleted ... press enter")
+input("Do you want to predict test with this model ? The current " + SUBMIT_FILE + " will be deleted ... press enter")
 print("predict and write...")
 labels_test_null, x_test = get_features(TEST_DATA_PATH, MAX_ITEMS, label_present=False)
 test_predicted_labels = search.predict(x_test)
